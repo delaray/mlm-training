a5a72ee0227c4bc2cfc079380bb0b0ee858d585c
Pierre de Lacaze <delaray@hotmail.com>
2025-03-06 16:30:56 +0100

    Initial commit

---
+++ src/ingest.py
@@ -0,0 +1,199 @@
+import re
+import os
+import itertools
+import pymupdf
+from typing import List, Dict
+from pptx import Presentation
+from statistics import mean
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+def flatten_chunks(documents_dict: dict) -> List[str]:
+
+    values_list = list(documents_dict.values())
+    flattened_list = list(itertools.chain(*values_list))
+
+    return flattened_list
+
+
+def read_files_directory(base_path: str,
+                         chunk_size: int,
+                         chunk_overlap: int) -> List[str]:
+    """
+    This function takes a directory path as input and reads every file in the directory
+    and its subdirectories using the ReadFile function.
+
+    Args:
+    directory_path (str): The path to the directory containing the files.
+
+    Returns:
+    dict: A dictionary where the keys are file paths and the values are the contents of the files.
+    """
+
+    loader = DocumentIngest(chunk_size=chunk_size,
+                            chunk_overlap=chunk_overlap)
+
+    files_content = {}
+
+    # Walk through the directory tree
+    for root, dirs, files in os.walk(base_path):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+
+            try:
+                # Read the file using the ReadFile function
+                content = loader.load_and_split(filepath)
+                files_content[filepath] = content
+                # print(f"Reading and storing {filepath}")
+            except Exception as e:
+                pass
+                # print(f"Error reading file {filepath}: {e}")
+
+    filtered_dict = {k: v for k, v in files_content.items() if v is not None}
+
+    values_list = list(filtered_dict.values())
+    flattened_list = list(itertools.chain(*values_list))
+
+    return flattened_list
+
+
+class DocumentIngest():
+
+    def __init__(self,
+                 chunk_size: int = 1100,
+                 chunk_overlap: int = 200):
+        self.text_splitter = self.create_text_splitter(chunk_size=chunk_size,
+                                                       chunk_overlap=chunk_overlap)
+
+    def create_text_splitter(self, chunk_size: int, chunk_overlap: int) -> RecursiveCharacterTextSplitter:
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            length_function=len,
+            is_separator_regex=False,
+            separators=["\n\n"]
+        )
+
+        return text_splitter
+
+    @staticmethod
+    def get_mean_line_length(text: str) -> int:
+        lines = text.split('\n')
+        word_count = [len(i.split(' ')) for i in lines]
+        return mean(word_count)
+
+    @staticmethod
+    def read_pdf_as_text(document_path: str) -> str:
+
+        doc = pymupdf.open(document_path)
+        n_pages = doc.page_count
+
+        # Extract all blocks from all pages
+        list_pages = [doc[i].get_text("blocks") for i in range(n_pages)]
+
+        # Filter by block
+        list_pages_clean_blocks = [
+            DocumentIngest.filter_blocks(i) for i in list_pages]
+
+        # Join all blocks per page
+        list_pages_clean_str = [''.join(i) for i in list_pages_clean_blocks]
+
+        # Filter by mean line length
+        list_pages_clean_str_filter = [
+            page for page in list_pages_clean_str if DocumentIngest.get_mean_line_length(page)]
+
+        # Filter by page content
+        list_pages_superclean = DocumentIngest.filter_pages(
+            list_pages_clean_str_filter)
+
+        # Join all pages
+        text = '\n'.join(list_pages_superclean)
+
+        return text
+
+
+@staticmethod
+def read_pptx_as_text(document_path: str) -> str:
+    presentation = Presentation(document_path)
+    extracted_texts = []
+
+    for slide_number, slide in enumerate(presentation.slides):
+
+        # Collect the texts from the components (shapes) of the slide
+        shapes_text = []
+        for shape in slide.shapes:
+            if hasattr(shape, "text"):
+                if shape.text.strip() != '':
+                    shapes_text.append(shape.text.strip())
+
+        # Remove trailing slide number from texts
+        shapes_text = shapes_text[:-1] if len(shapes_text) > 0 else shapes_text
+
+        # Append the extracted text to the list of extracted texts
+        if shapes_text != []:
+            extracted_texts.append(shapes_text)
+        else:
+            # Insert None placeholders so we know slide number
+            # (i.e. index) and number of empty slides
+            extracted_texts.append('')
+
+    # Return the extracted text from all slides as a single string
+    extracted_text = ' '.join([' '.join(strs) for strs in extracted_texts])
+    extracted_text = extracted_text.replace('\n', ' ')
+
+    return extracted_text
+
+
+@staticmethod
+def filter_pages(list_pages: List[str]) -> List[str]:
+
+    list_pages_clean = [page for page in list_pages if len(page) > 0]
+    list_pages_clean = [page for page in list_pages_clean if not bool(
+        re.search("Intentionally Blank", page))]
+
+    return list_pages_clean
+
+
+@staticmethod
+def filter_blocks(list_blocks: List[tuple],
+                  y_diff: int = 400,
+                  y_min: int = 55,
+                  y_max: int = 735,
+                  min_len: int = 5) -> List[str]:
+
+    list_blocks_clean = [i for i in list_blocks if i[3]-i[1] < y_diff]
+    list_blocks_clean = [i for i in list_blocks_clean if i[3] > y_min]
+    list_blocks_clean = [i for i in list_blocks_clean if i[3] < y_max]
+    list_blocks_clean = [i for i in list_blocks_clean if len(i[4]) > min_len]
+
+    list_text = [i[4] for i in list_blocks_clean]
+    list_text_clean = [
+        i for i in list_text if not bool(re.search('^Figure', i))]
+    list_text_clean = [
+        i for i in list_text_clean if not bool(re.search('^Table', i))]
+
+    if len(list_text_clean) > 1:
+        return list_text_clean
+    else:
+        return []
+
+
+def load_and_split(self, document_path: str) -> List[str]:
+    # regex = (\d{7})_{0,1}\d{0,2}\.(pdf|pptx)
+
+    try:
+        if '.pdf' in document_path:
+            text = DocumentIngest.read_pdf_as_text(document_path)
+            # print (f"{document_path} document has been split and loaded")
+        else:
+            pass
+            # print (f"{document_path} format not taken into account, document is not parsed")
+
+        text_chunks = self.text_splitter.split_text(text)
+
+        return text_chunks
+
+    except:
+        pass
+        # print (f"error reading {document_path}, document not parsed ")
---
+++ src/train.py
@@ -0,0 +1,404 @@
+import os
+import itertools
+import pickle
+import datetime
+import argparse
+import logging
+from datetime import date, datetime
+from itertools import chain
+from typing import List, Union
+import matplotlib.pyplot as plt
+
+import torch
+from datasets import Dataset
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+from transformers import Trainer, TrainingArguments
+from transformers import DataCollatorForLanguageModeling
+from transformers import logging as transformers_logging
+
+from mlm.src.preprocess_data import group_texts
+from mlm.src.document_ingest import DocumentIngest, read_files_directory
+from mlm.src.document_ingest import flatten_chunks
+
+# ------------------------------------------------------------------------------
+
+# Global Defaults & Settings
+
+# ------------------------------------------------------------------------------
+
+DEFAULT_DATA_DIR = 'data'
+DEFAULT_RESULTS_DIR = 'results'
+DEFAULT_LOGS_DIRECTORY = 'logs'
+DEFAULT_MODELS_DIR = 'models'
+DEFAULT_MODEL_NAME = 'bert-base-uncased'
+
+DEFAULT_MAX_TOKEN_LENGTH = 512
+DEFAULT_MAX_CHUNK_SIZE = 4 * DEFAULT_MAX_TOKEN_LENGTH
+
+DEFAULT_EPOCHS = 30
+DEFAULT_BATCH_SIZE = 32
+DEFAULT_LEARNING_RATE = 0.0002
+DEFAULT_WEIGHT_DECAY = 0.004
+
+folder_intouch_docs = 'intouch_documents_consolidated'
+folder_intouch_docs_path = os.path.join(DEFAULT_DATA_DIR, folder_intouch_docs)
+
+max_token_length = 512
+max_chunk_size = max_token_length * 4
+chunk_overlap = 0
+
+today = str(date.today())
+
+model_name = DEFAULT_MODEL_NAME
+model_path = os.path.join(DEFAULT_MODELS_DIR, model_name)
+model_save_path = f"{model_path}-trained-{today}"
+
+DEFAULT_TOKENIZER = AutoTokenizer.from_pretrained(model_path)
+DEFAULT_MODEL = AutoModelForMaskedLM.from_pretrained(model_path)
+
+MODEL = DEFAULT_MODEL
+TOKENIZER = DEFAULT_TOKENIZER
+
+
+# ------------------------------------------------------------------------------
+# Logging Setup
+# ------------------------------------------------------------------------------
+
+def initialize_logging(model_name, epochs, logs_path=DEFAULT_LOGS_DIRECTORY):
+
+    logger = logging.getLogger(__name__)
+
+    train_date = datetime.now().strftime('%Y-%m-%d')
+    log_filename = datetime.now().strftime(
+        f"train-log-{model_name}-{train_date}-epochs-{epochs}.log")
+
+    log_filepath = os.path.join(logs_path, log_filename)
+
+    logging.basicConfig(
+        encoding='utf-8',
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        handlers=[
+            logging.FileHandler(log_filepath, mode='w'),
+            logging.StreamHandler()
+        ]
+    )
+
+    transformers_logging.set_verbosity_info()
+    transformers_logging.enable_default_handler()
+    transformers_logging.enable_explicit_format()
+
+    return logger
+
+
+def print_and_log(msg: str):
+    """Print <msg> to console and log <msg> to log file"""
+    print(msg)
+    logging.info(msg)
+
+
+# ------------------------------------------------------------------------------
+# Data Preparation
+# ------------------------------------------------------------------------------
+
+
+def tokenize_function(examples, tokenizer=DEFAULT_TOKENIZER):
+    return tokenizer(examples["text"],
+                     truncation=True,
+                     max_length=max_token_length,
+                     return_overflowing_tokens=True)
+
+
+def prepare_datasets(data_path=DEFAULT_DATA_DIR, max_chunks: Union[int | None] = None) -> dict:
+
+    data_prep_start_time = datetime.now()
+    logging.info("Preparing data sets")
+
+    # Load data
+    chunks = read_files_directory(
+        data_path, chunk_size=max_chunk_size, chunk_overlap=0)
+    chunks = chunks[:max_chunks] if max_chunks is not None else chunks
+
+    if len(chunks) > 0:
+        data_dict = {"text": chunks}
+
+        dataset = Dataset.from_dict(data_dict)
+        dataset = dataset.train_test_split(test_size=0.1, seed=2024)
+
+        lm_datasets = dataset.map(tokenize_function,
+                                  batched=True,
+                                  num_proc=4,
+                                  remove_columns=["text"])
+
+        # Total number of chunks
+        n_chunks = len(lm_datasets['train']) + len(lm_datasets['test'])
+
+        # Log dataset dimensions
+        logging.info(f"A total of {n_chunks} have been prepared")
+        logging.info(f"The max length tokens is {max_token_length}")
+        logging.info(f"{len(lm_datasets['train'])} will be used for training")
+
+        # Log total data preparation time
+        data_prep_end_time = datetime.now()
+        logging.info(
+            f"Data preparation took {data_prep_end_time - data_prep_start_time}")
+
+        return lm_datasets, chunks
+
+    else:
+        logging.info(
+            'The list of chunks is empty, there is no dataset to prepare.')
+        return None, []
+
+
+def get_training_args(model_name=DEFAULT_MODEL_NAME,
+                      epochs: float = DEFAULT_EPOCHS,
+                      batch_size: int = DEFAULT_BATCH_SIZE,
+                      learning_rate: float = DEFAULT_LEARNING_RATE,
+                      weight_decay: float = DEFAULT_WEIGHT_DECAY):
+
+    # Divide batch size by number of GPU’s
+    per_device_batch_size = int(batch_size / 4)
+
+    training_args = TrainingArguments(
+        model_name,
+        eval_strategy="epoch",
+        logging_strategy="epoch",
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+        push_to_hub=False,
+        num_train_epochs=epochs,
+        per_device_train_batch_size=per_device_batch_size,
+        per_device_eval_batch_size=8,
+    )
+
+    return training_args
+
+
+class CustomTrainer(Trainer):
+
+    def compute_loss(self, *args, **kwargs):
+        # Perform evaluation
+        result = super().compute_loss(*args, **kwargs)
+        # Log Training Loss
+        # print_and_log(f"Epoch {self.state.epoch}: Train Loss: {result[0]}")
+        return result
+
+    def evaluate(self, *args, **kwargs):
+        # Perform evaluation
+        result = super().evaluate(*args, **kwargs)
+        # Log evaluation Loss
+        print_and_log(
+            f"Epoch {self.state.epoch}: Eval loss: {result['eval_loss']}")
+        return result
+
+
+def get_trainer(model, training_args, lm_datasets, data_collator):
+    trainer = CustomTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=lm_datasets["train"],
+        eval_dataset=lm_datasets["test"],
+        data_collator=data_collator
+    )
+
+    return trainer
+
+# ------------------------------------------------------------------------------
+# Model Training
+# ------------------------------------------------------------------------------
+
+
+def train_model(model_name, model, data_collator, datasets, epochs=25,
+                batch_size: int = DEFAULT_BATCH_SIZE,
+                learning_rate: float = DEFAULT_LEARNING_RATE,
+                weight_decay: float = DEFAULT_WEIGHT_DECAY):
+
+    training_start_time = datetime.now()
+    print_and_log("\nPreparing model training with the following parameters:")
+    print_and_log(f"Epochs: {epochs}\nBatch size: {batch_size}")
+    print_and_log(
+        f"Learning rate: {learning_rate}\nWeight decay: {weight_decay}\n")
+
+    print_and_log("\nSetting up training args and trainer.")
+    training_args = get_training_args(model_name,
+                                      epochs=epochs,
+                                      batch_size=batch_size,
+                                      learning_rate=learning_rate,
+                                      weight_decay=weight_decay)
+
+    trainer = get_trainer(model, training_args, datasets, data_collator)
+
+    try:
+        print_and_log("\nRunning model training...\n")
+        trainer.train()
+
+        training_end_time = datetime.now()
+        logging.info("\nModel training ended.\n")
+        print_and_log(
+            f"Training took {training_end_time - training_start_time}")
+
+        return trainer
+
+    except Exception as e:
+        print_and_log(f"\nError in train_model {model_name}\n{e}\n")
+        return trainer
+
+# ------------------------------------------------------------------------------
+# Load and Save Model
+# ------------------------------------------------------------------------------
+
+
+def save_model(model_name, trainer, tokenizer, models_dir=DEFAULT_MODELS_DIR):
+
+    train_date = datetime.now().strftime('%Y-%m-%d')
+    dir_name = f"{model_name}-trained-{train_date}"
+    dir_pathname = os.path.join(models_dir, dir_name)
+
+    trainer.save_model(dir_pathname)
+    tokenizer.save_pretrained(dir_pathname)
+
+    model_saved_time = datetime.now()
+    print_and_log(
+        f"Model & tokenizer saved to directory: {dir_pathname} at {model_saved_time}")
+
+
+def load_model(model_name, model_save_path=DEFAULT_MODELS_DIR):
+    model_path = os.path.join(model_save_path, model_name)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model = AutoModelForMaskedLM.from_pretrained(model_path)
+
+    return model, tokenizer
+
+
+# ------------------------------------------------------------------------------
+# Save & Load Training History
+# ------------------------------------------------------------------------------
+
+# NB: History is in trainer.state.log_history as a list of dictionaries
+
+
+def training_history_file(model_name, epochs):
+    today = str(date.today())
+    return f"training-history-{model_name}-trained-{today}-{epochs}.pkl"
+
+
+def save_training_history(trainer, model_name, epochs, path='results'):
+    history = trainer.state.log_history
+    pathname = os.path.join(path, training_history_file(model_name, epochs))
+    with open(pathname, 'wb') as f:
+        pickle.dump(history, f)
+    print_and_log(f"Training history saved to {pathname}")
+
+
+def load_training_history(model_name, epochs, path='results'):
+    pathname = os.path.join(path, training_history_file(model_name, epochs))
+    with open(pathname, 'rb') as f:
+        history = pickle.load(f)
+    return history
+
+# ------------------------------------------------------------------------------
+
+
+def plot_training_loss(log_history: list[dict]):
+    steps = []
+    train_loss = []
+    eval_loss = []
+
+    for entry in log_history:
+        if 'loss' in entry and 'step' in entry:
+            steps.append(entry['step'])
+            train_loss.append(entry['loss'])
+        if 'eval_loss' in entry:
+            eval_loss.append(entry['eval_loss'])
+
+    plt.figure(figsize=(10, 6))
+    if train_loss:
+        plt.plot(steps, train_loss, label="Training Loss", marker='o')
+    if eval_loss:
+        plt.plot(steps, eval_loss, label="Evaluation Loss", marker='x')
+
+    plt.xlabel("Steps")
+    plt.ylabel("Loss")
+    plt.title("Training & Evaluation Loss")
+    plt.legend()
+    plt.grid(True)
+
+    plt.show()
+
+# ------------------------------------------------------------------------------
+# CLI Arguments
+# ------------------------------------------------------------------------------
+
+
+parser = argparse.ArgumentParser(description="Training module for MLM")
+
+parser.add_argument('name', metavar='name', type=str,
+                    help='The name of the model to train.')
+
+parser.add_argument('--chunks', metavar='chunks', type=int, default=None,
+                    help='The number of chunks to use or all chunks if unspecified.')
+
+parser.add_argument('--epochs', metavar='epochs', type=int, default=20,
+                    help='The number of epochs to use for training. Default is 20.')
+
+parser.add_argument('--rate', metavar='rate', type=float, default=4e-5,
+                    help='The training rate to use during training. The default is 4e-5.')
+
+parser.add_argument('--decay', metavar='decay', type=float, default=0.002,
+                    help='The weight decay to use during training. The default is 0.002.')
+
+parser.add_argument('--batch-size', metavar='batch_size', type=int, default=16,
+                    help='The batch size to use during training. Default is 16.')
+
+# ------------------------------------------------------------------------------
+# Entry function for Model Training
+# ------------------------------------------------------------------------------
+
+
+def run_model_training(model_name: str, tokenizer, model,
+                       data_path=DEFAULT_DATA_DIR,
+                       epochs: Union[int | None] = 30,
+                       batch_size: int = DEFAULT_BATCH_SIZE,
+                       learning_rate: float = DEFAULT_LEARNING_RATE,
+                       weight_decay: float = DEFAULT_WEIGHT_DECAY,
+                       max_chunks: Union[int | None] = None,
+                       models_dir=DEFAULT_MODELS_DIR):
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    try:
+        start_time = datetime.now()
+        print_and_log(f"Training script started at {start_time}")
+        logging.info(f"Training model {model_name}")
+
+        # Prepare Training data & data collator
+        datasets, _ = prepare_datasets(
+            data_path=data_path, max_chunks=max_chunks)
+        data_collator = DataCollatorForLanguageModeling(
+            tokenizer=tokenizer, mlm_probability=0.15)
+
+        # Train the model with specified parameters
+        trainer = train_model(model_name, model, data_collator, datasets,
+                              epochs=epochs, batch_size=batch_size,
+                              learning_rate=learning_rate, weight_decay=weight_decay)
+
+        # Save model, tokenizer & training history
+        save_model(model_name, trainer, tokenizer, models_dir=models_dir)
+        save_training_history(trainer, model_name, epochs, path="results")
+
+        # Return the trainer object
+        return trainer
+
+    except Exception as e:
+        print_and_log(f"\nError in run_training_model {model_name}\n{e}\n")
+        return trainer
+
+
+# ------------------------------------------------------------------------------
+# End of File
+# ------------------------------------------------------------------------------
